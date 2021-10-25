43369509eb23b9e6bb7b644eac7203b9c51d35c9
Sergey Akimov <qqun.y@yandex.ru>
2021-10-25 15:35:39 +0300

    Moved borders of sought values to constants

--- matrices/task5.cpp
+++ matrices/task5.cpp
@@ -3,12 +3,14 @@
 #include "matrix.h"
 
 bool FindDecomposition(const Matrix& a, Matrix& b, Matrix& c) {
-    for (int64_t a1 = -5; a1 <= 5; ++a1) {
-        for (int64_t a2 = -5; a2 <= 5; ++a2) {
-            for (int64_t a3 = -5; a3 <= 5; ++a3) {
-                for (int64_t b1 = -5; b1 <= 5; ++b1) {
-                    for (int64_t b2 = -5; b2 <= 5; ++b2) {
-                        for (int64_t b3 = -5; b3 <= 5; ++b3) {
+    const int64_t MIN_VAL = -5;
+    const int64_t MAX_VAL = 5;
+    for (int64_t a1 = MIN_VAL; a1 <= MAX_VAL; ++a1) {
+        for (int64_t a2 = MIN_VAL; a2 <= MAX_VAL; ++a2) {
+            for (int64_t a3 = MIN_VAL; a3 <= MAX_VAL; ++a3) {
+                for (int64_t b1 = MIN_VAL; b1 <= MAX_VAL; ++b1) {
+                    for (int64_t b2 = MIN_VAL; b2 <= MAX_VAL; ++b2) {
+                        for (int64_t b3 = MIN_VAL; b3 <= MAX_VAL; ++b3) {
                             b[0][1] = a1;
                             b[1][1] = a2;
                             b[2][1] = a3;
