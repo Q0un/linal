706bf4240911961a23b3e25bb6552112970296ae
Q0un <qqun.y@yandex.ru>
2021-12-07 22:14:23 +0300

    Solved task5

---
+++ IDZ2/task5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "matrix.h"
+#include "matrix_utils.h"
+
+int main() {
+    Matrix a = {{1,  -2},
+                {-3, 6},
+                {4,  -8},
+                {-2, 4},
+                {2,  -4}};
+    Matrix b = {{1, 3, 2, -3, -1},
+                {3, 1, 2, -1, -3}};
+    std::cout << GetCharPoly(a * b) << std::endl;
+    return 0;
+}
